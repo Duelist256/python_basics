bebed36a4ee4febb00c684183af7a21f903910c0
Duelist256 <selykov.iw@yandex.ru>
2019-04-05 00:56:21 +0300

    Input

---
+++ python_basics/input.py
@@ -0,0 +1,13 @@
+name = input("What's your name? ")
+print("Hello, %s!" % name)
+print()
+job = input("What's your job? ")
+print("Your job is %s" % job)
+print()
+
+# Exercise 1. Make a program that asks a phone number.
+phoneNumber = input("Input a phone number: ")
+print("The phone number is : %s" % phoneNumber)
+# Exercise 2. Make a program that asks the users preferred programming language.
+pl = input("What is your preferred programming language: ")
+print("%s is cool" % pl)
